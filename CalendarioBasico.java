5796a0761ef481645bd028b22dcd5a73e3ce5444
Maikel <maikel_20@hotmail.es>
2017-10-30 23:34:43 +0100

    Primer commit

---
+++ CalendarioBasico.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class CalendarioBasico here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CalendarioBasico
+{
+    // instance variables - replace the example below with your own
+    private int day;
+    private int month;
+    private int year;
+    
+    /**
+     * Constructor for objects of class CalendarioBasico
+     */
+    public CalendarioBasico()
+    {
+    }
+    
+    /**
+     * 
+     */
+    public void IntroduceDate(int dia, int mes, int ano)
+    {
+        day = dia;
+        month = mes;
+        year = ano;
+    }
+    
+}
